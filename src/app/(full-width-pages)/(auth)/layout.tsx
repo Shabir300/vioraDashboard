09e8972e99d8d1c45e480b29de7f0803df0226df
Your Name <you@example.com>
2025-08-14 13:54:43 +0500

    logo text

--- src/app/(full-width-pages)/(auth)/layout.tsx
+++ src/app/(full-width-pages)/(auth)/layout.tsx
@@ -30,7 +30,7 @@ export default function AuthLayout({
                   />
                 </Link>
                 <p className="text-center text-gray-400 dark:text-white/60">
-                  Free and Open-Source Tailwind CSS Admin Dashboard Template
+                 The Future of Client Management for Agencies
                 </p>
               </div>
             </div>
